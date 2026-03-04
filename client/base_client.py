9309b77b6803e4244c4f255b993280efce94c3cd
deniszalutskiy <mr.booryat@gmail.com>
2026-03-04 20:53:13 +1000

    метод обновления токена

--- client/base_client.py
+++ client/base_client.py
@@ -1,12 +1,13 @@
 import json
-import logging
-import sys
 
 import requests
 from requests import Response
 
 from config import settings
-from tests.conftest import logger
+from models.Users.refresh_token_response import RefreshTokenResponse
+from models.Users.user_login_response import UserLoginResponse
+from utils.logger import logger
+from utils.support import check_status_code
 
 
 class BaseClient:
@@ -26,11 +27,15 @@ class BaseClient:
         logger.info(f"--> {method} {url}")
 
         if kwargs.get("json"):
-            logger.info(f"Payload: {kwargs['json']}")
+            logger.info(f"Body: {kwargs['json']}")
         if kwargs.get("params"):
             logger.info(f"Params: {kwargs['params']}")
         try:
             response = self.session.request(method, url, **kwargs)
+            if response.status_code == 401 and "/refresh-token" not in endpoint:
+                logger.warning("AccessToken истёк")
+                self.refresh_token()
+                response = self.session.request(method, url, **kwargs)
             logger.info(f"<-- Status: {response.status_code}")
             content_type = response.headers.get("Content-Type", "")
 
@@ -44,3 +49,27 @@ class BaseClient:
         except Exception as e:
             logger.error(e)
             raise e
+
+    def auth(self, body: dict) -> UserLoginResponse:
+        """
+        Авторизует пользователя и сохраняет токен в сессию
+        :param body: Креды пользователя
+        :return: словарь с ответом от сервиса
+        """
+        response = self._request("POST", "users/login", json=body)
+        check_status_code(response.status_code, 200)
+        login_data = UserLoginResponse.model_validate(response.json())
+        access_token = login_data.data.access_token
+        self.session.headers.update({"Authorization": f"Bearer {access_token}"})
+        return login_data
+
+    def refresh_token(self):
+        """Обновление токена"""
+        endpoint = "users/refresh-token"
+        response = self._request("POST", endpoint)
+        if response.status_code == 200:
+            refresh_data = RefreshTokenResponse.model_validate(response.json())
+            self.session.headers.update({"Authorization": f"Bearer {refresh_data.data.access_token}"})
+        else:
+            logger.error("Не удалось обновить accessToken")
+            raise Exception("Токен не обновлён")
--- config.py
+++ config.py
@@ -1,12 +1,16 @@
 import os.path
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import Field
 
 env_path = os.path.join(os.path.dirname(__file__), ".env")
 
 class Settings(BaseSettings):
-    base_url: str = Field(validation_alias="BASE_URL")
+    base_url: str
+    db_url: str
+    username: str
+    password: str
+    service_username: str
+    service_password: str
     model_config = SettingsConfigDict(env_file=env_path)
 
 settings = Settings()  # noqa
