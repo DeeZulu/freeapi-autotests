0dd773a3bd2f90807999982d777873ce4d5aec9e
deniszalutskiy <mr.booryat@gmail.com>
2026-03-05 19:13:57 +1000

    фикстура клиента с авторизацией

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,27 @@
-import logging
-import sys
+
+from time import time
+
+from pytest import fixture
+
+from client.user_client import UserClient
+from config import settings
+from models.Users.user_register_request import UserRegisterRequest
 
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s | %(levelname)s | %(message)s",
-    datefmt="%H:%M:%S",
-    handlers=[logging.StreamHandler(sys.stdout)]
-)
-logger = logging.getLogger(__name__)
+@fixture(scope="session")
+def user_client():
+    """Клиент для запросов, требующих авторизацию"""
+    client = UserClient()
+    user = {"password": settings.service_password, "username": settings.service_username}
+    client.auth(user)
+    return client
+
+@fixture(scope="class")
+def test_user():
+    """Cоздание тестового пользователя"""
+    current_time = int(time())
+    return UserRegisterRequest(
+        email=f"{current_time}@gmail.com",
+        password=settings.password,
+        role="ADMIN",
+        username=f"{settings.username}_{current_time}")
