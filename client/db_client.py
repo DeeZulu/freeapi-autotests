1a2a64b19ce68b0fda6350fa32411d97da7a03c9
deniszalutskiy <mr.booryat@gmail.com>
2026-03-13 00:40:10 +1000

    докстринги и методы удаления и получения пользователя

--- client/db_client.py
+++ client/db_client.py
@@ -8,5 +8,24 @@ class DbClient:
         self.client = MongoClient(settings.db_url)
         self.db = self.client["freeapi"]
 
-    def get_all_users(self):
+    def get_all_users(self) -> list:
+        """
+        Получение всех пользователей
+        :return Список пользователей
+        """
         return list(self.db.users.find({}))
+
+    def delete_user(self, username: str):
+        """
+        Удаление пользователя
+        :param username
+        """
+        return self.db.users.delete_one({"username": username})
+
+    def get_user(self, username: str) -> dict:
+        """
+        Получение пользователя
+        :param username Имя пользователя
+        :return Пользователь
+        """
+        return self.db.users.find_one({"username": username})
