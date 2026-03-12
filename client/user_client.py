1de98ca17b8e5c3de35b7d737ff8dbe9e9fb186f
deniszalutskiy <mr.booryat@gmail.com>
2026-03-13 00:39:27 +1000

    метод login и докстринги

--- client/user_client.py
+++ client/user_client.py
@@ -1,11 +1,9 @@
 from requests.models import Response
 
 from client.base_client import BaseClient
-from models.Users.refresh_token_response import RefreshTokenResponse
 from models.Users.user_logout_response import UserLogoutResponse
 from models.Users.user_register_request import UserRegisterRequest
 from models.Users.user_register_response import UserRegisterResponse
-from utils.support import check_status_code
 
 
 class UserClient(BaseClient):
@@ -20,15 +18,30 @@ class UserClient(BaseClient):
         response = self._request("POST", endpoint, json=body.model_dump(), refresh=True)
         return UserRegisterResponse.model_validate(response.json())
 
-    def login(self, body: dict):
+    def login(self, body: dict, refresh=False) -> Response:
         """
         Логин пользователя
-        :param body: Креды для авторизации
+        :param body Креды для авторизации
+        :param refresh Надо ли обновлять accessToken в случае 401 ответа
         """
-        pass
+        endpoint = f"{self.user_path}/login"
+        response = self._request("POST", endpoint, refresh, json=body)
+        return response
 
     def logout(self, refresh=False) -> Response:
-        """Логаут пользователя"""
+        """
+        Логаут пользователя
+        :param refresh Надо ли обновлять accessToken в случае 401 ответа
+        """
         endpoint = f"{self.user_path}/logout"
-        response = self._request("POST", endpoint, refresh)
-        return response
+        return self._request("POST", endpoint, refresh)
+
+    def verify_email_by_token(self, verification_token: str, refresh=True):
+        """
+        Проверка адреса электронной почты по верификационному токену
+        :param verification_token Токен верификации
+        :param refresh Надо ли обновлять accessToken в случае 401 ответа
+        """
+        endpoint = f"{self.user_path}/verify-email/{verification_token}"
+        return self._request("GET", endpoint, refresh)
+
